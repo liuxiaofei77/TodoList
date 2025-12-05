d33812a9ebb85417f3f37f720b4f429ade429bdd
LXF <982572176@qq.com>
2025-12-05 17:38:57 +0800

    first push

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css' // 全局样式
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus) // 全局注册所有组件
+
+app.mount('#app')
